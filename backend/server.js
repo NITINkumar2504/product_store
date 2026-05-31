fe320a36b77c68c9150b55f230d7c2d692efdc4e
Nitin Kumar <nk9465110@gmail.com>
2026-05-31 23:26:06 +0530

    deployment code changes

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import path from 'path' 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000
+const __dirname = path.resolve();
 
 app.use(express.json()); // allow us to accept JSON data in req.body
 
 import userRouter from './routes/product.routes.js'
 app.use('/api/products', userRouter)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+    app.get('/*path', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => {
     connectDB();
